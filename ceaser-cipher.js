85a70a6dac02739d1238fa073c66bf9efc7bec4a
dante <jamesmidzi@gmail.com>
2024-01-10 17:11:37 +0200

    cipher with char code

--- ceaser-cipher.js
+++ ceaser-cipher.js
@@ -50,5 +50,30 @@ function rot13(str) {
 
 	return decodedStr;
 }
-console.log(rot13("SERR PBQR PNZC"));
+
+/* CharCode Version */
+const ROTATE = 13;
+
+function rot13Code(str) {
+	let decodedStr = "";
+
+	for (let i = 0; i < str.length; i++) {
+		const char = str[i];
+		const charCode = str.charCodeAt(i);
+
+		if (/[A-M]/.test(char)) {
+			const decodedChar = charCode + ROTATE;
+			decodedStr += String.fromCharCode(decodedChar);
+		} else if (/[N-Z]/.test(char)) {
+			const decodedChar = charCode - ROTATE;
+			decodedStr += String.fromCharCode(decodedChar);
+		} else {
+			decodedStr += char;
+		}
+	}
+
+	return decodedStr;
+}
+
+console.log(rot13Code("SERR PBQR PNZC"));
 // rot13("SERR PBQR PNZC");
