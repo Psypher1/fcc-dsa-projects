a2ee0905bdfcce4282cbff953b69c93074b79ebc
dante <jamesmidzi@gmail.com>
2024-01-10 14:24:23 +0200

    staged of cipher

--- ceaser-cipher.js
+++ ceaser-cipher.js
@@ -1,7 +1,54 @@
+/* 
+A: N
+B: O
+ */
+
+const CHAR_MAP = {
+	A: "N",
+	B: "O",
+	C: "P",
+	D: "Q",
+	E: "R",
+	F: "S",
+	G: "T",
+	H: "U",
+	I: "V",
+	J: "W",
+	K: "X",
+	L: "Y",
+	M: "Z",
+	N: "A",
+	O: "B",
+	P: "C",
+	Q: "D",
+	R: "E",
+	S: "F",
+	T: "G",
+	U: "H",
+	V: "I",
+	W: "J",
+	X: "K",
+	Y: "L",
+	Z: "M",
+};
+
 function rot13(str) {
-	return str;
+	let decodedStr = "";
+
+	for (let i = 0; i < str.length; i++) {
+		// decodedStr += str[i];
+		// if char is a letter, add decoded letter
+		if (str[i] === "A") {
+			// decode letter
+			// add decoded letter to string
+			decodedStr += "N";
+		} else {
+			// else add char as is
+			decodedStr += str[i];
+		}
+	}
+
+	return decodedStr;
 }
-
-function evalutate() {}
-
-rot13("SERR PBQR PNZC");
+console.log(rot13("A"));
+// rot13("SERR PBQR PNZC");
