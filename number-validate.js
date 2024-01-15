7487d34b0d8675b9e0105be10b808fd54b9f4b9d
dante <jamesmidzi@gmail.com>
2024-01-15 23:52:00 +0200

    refactored number val

--- number-validate.js
+++ number-validate.js
@@ -1,15 +1,15 @@
 // const RE = /^\d\d\d-\d\d\d-\d\d\d\d$/;
-const RE = /^\d{3}-\d{3}-\d{4}$/;
-const RE2 = /^\(\d{3}\)\s?\d{3}-\d{4}$/;
-const RE3 = /^\d{3}\s?\d{3}\s?\d{4}$/;
+const RE = /^(1\s?)?\d{3}-\d{3}-\d{4}$/;
+const RE2 = /^(1\s?)?\(\d{3}\)\s?\d{3}-\d{4}$/;
+const RE3 = /^(1\s?)?\d{3}\s?\d{3}\s?\d{4}$/;
 
-const RE4 = /^\d{10}$/;
-const RE5 = /^1\s\d{3}\s\d{3}\s\d{4}$/;
-const RE6 = /^1\s\d{3}-\d{3}-\d{4}$/;
-const RE7 = /^1\s?\(\d{3}\)\s?\d{3}-\d{4}$/;
+// const RE4 = /^\d{10}$/;
+// const RE5 = /^1\s\d{3}\s\d{3}\s\d{4}$/;
+// const RE6 = /^1\s\d{3}-\d{3}-\d{4}$/;
+// const RE7 = /^1\s?\(\d{3}\)\s?\d{3}-\d{4}$/;
 
 // create array of regex
-const REGEXES = [RE, RE2, RE3, RE5, RE6, RE7];
+const REGEXES = [RE, RE2, RE3];
 
 function telephoneCheck(str) {
 	// loop through regexes
